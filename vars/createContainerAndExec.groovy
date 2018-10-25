30d2ae03ecada389473422957af4121a69bde6b4
Dmitriy <Bichenko>
2018-10-25 14:11:47 +0300

    Script fix

--- vars/createContainerAndExec.groovy
+++ vars/createContainerAndExec.groovy
@@ -1,13 +1,13 @@
 def call(execScript, image, containerName, workspaceDirectory ){
 
     def shellScript = '''
-        docker stop $containerName || true
-        docker rm $containerName || true
+        docker stop ${containerName} || true
+        docker rm ${containerName} || true
     
-        docker run -d --name $containerName --volume=$workspaceDirectory:/workspace $image
+        docker run -d --name ${containerName} --volume=${workspaceDirectory}:/workspace ${image}
     
-        docker exec $containerName bash -c \"$execScript\"
-        docker stop $containerName
+        docker exec ${containerName} bash -c \"${execScript}\"
+        docker stop ${containerName}
     '''
     sh shellScript;
 

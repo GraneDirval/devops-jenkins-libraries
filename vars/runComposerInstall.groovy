b3ac0b955f261001673e4e93abc62a35ef21a244
Dmitriy <Bichenko>
2018-10-25 14:18:46 +0300

    Script adjustment

--- vars/runComposerInstall.groovy
+++ vars/runComposerInstall.groovy
@@ -1,3 +1,3 @@
-def call(){
-    sh 'composer install --ignore-platform-reqs --no-scripts --classmap-authoritative --optimize-autoloader --no-suggest'
+def call(directory){
+    sh "composer install --ignore-platform-reqs --no-scripts --classmap-authoritative --optimize-autoloader --no-suggest -d=${directory}"
 }

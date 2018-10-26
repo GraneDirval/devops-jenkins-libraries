93d20a77c3d5c8e9a56e25513317ae6015c1dac4
Dmitriy <Bichenko>
2018-10-26 13:29:28 +0300

    Slack function

--- vars/slackCreateAttachment.groovy
+++ vars/slackCreateAttachment.groovy
@@ -1,16 +1,17 @@
 import groovy.json.*
 
-def call(fallback, color, fields) {
-    def attachmentPayload = [
-            [
-                    fallback: fallback,
-                    color   : color,
-                    fields  : fields
-            ]]
+def call(pretext, fallback, color, fields) {
+  def attachmentPayload = [
+      [
+          pretext : pretext,
+          fallback: fallback,
+          color   : color,
+          fields  : fields
+      ]]
 
 
 
 
-    return new JsonBuilder(attachmentPayload).toPrettyString();
+  return new JsonBuilder(attachmentPayload).toPrettyString();
 
 }

a62bfaa0745d70b37d5b368a422b85e24b517f8f
Dmitriy Bichenko <example@gmail.com>
2018-11-17 15:01:32 +0200

    Debug message

--- vars/writeToExternalJsonFile.groovy
+++ vars/writeToExternalJsonFile.groovy
@@ -2,6 +2,6 @@ import groovy.json.JsonOutput
 
 def call(filePath, content) {
     def json = JsonOutput.toJson(content)
-    sh "echo $json>$filePath"
+    sh "echo \"$json\">$filePath"
     sh "cat $filePath"
 }

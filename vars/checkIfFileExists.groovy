c65e64def68b94bcc586c921f9ae72be5de7038b
Dmitriy Bichenko <example@gmail.com>
2018-11-17 14:32:30 +0200

    File exist function

--- vars/checkIfFileExists.groovy
+++ vars/checkIfFileExists.groovy
@@ -1,6 +1,6 @@
 Boolean call(filePath){
 
-    def res = sh(script: "test -d ${filePath} && echo '1' || echo '0' ", returnStdout: true).trim()
+    def res = sh(script: "test -f ${filePath} && echo '1' || echo '0' ", returnStdout: true).trim()
 
     boolean exists = (res == 1)
 

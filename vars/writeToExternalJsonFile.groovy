136617dd2d72aa7bbfca6aee11a978a9b409a352
Dmitriy <Bichenko>
2018-11-19 10:17:52 +0200

    File write adjustment

--- vars/writeToExternalJsonFile.groovy
+++ vars/writeToExternalJsonFile.groovy
@@ -2,10 +2,5 @@ import groovy.json.JsonOutput
 
 def call(filePath, content) {
     def json = JsonOutput.toJson(content)
-
-    def shellScript = "echo '$json'>$filePath"
-    shellScript.execute()
-
-    String resolvedContent = ("cat $filePath".execute().text)
-    print resolvedContent
+    sh "echo '$json'>$filePath"
 }

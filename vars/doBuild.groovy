30af9297d84e5165237bc1553d4f89fcbfa1ad7c
Dmitriy Bichenko <example@gmail.com>
2018-11-24 22:33:30 +0200

    Helper function

---
+++ vars/doBuild.groovy
@@ -0,0 +1,11 @@
+def call(appId, commitHash, appDirectory, buildVariables) {
+    def stageBuild = build job: 'ci-build-git-revision', propagate: false, wait: true, parameters: [
+            string(name: 'BILLING_API_HOST', value: buildVariables.BILLING_API_HOST),
+            booleanParam(name: 'DROP_DB_ON_EACH_COMMIT', value: buildVariables.DROP_DB_ON_EACH_COMMIT),
+            [$class: 'GitParameterValue', name: 'COMMIT_HASH', value: commitHash],
+            string(name: 'APP_ID', value: appId),
+            string(name: 'DIRECTORY_NAME', value: appDirectory),
+    ];
+
+    return stageBuild;
+}

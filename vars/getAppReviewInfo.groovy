da5d2dae47c4aaa23148b01991281cc0b65c15dc
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-09-11 17:05:56 +0300

    Additional functionality

---
+++ vars/getAppReviewInfo.groovy
@@ -0,0 +1,10 @@
+@NonCPS
+def call (APP_ID){
+
+  GString shellScript = "cat /var/app/${APP_ID}/config/review.json"
+
+  def result = (shellScript.execute().text)
+  def reviewInfo = new JsonSlurper().parseText(result)
+
+  return reviewInfo
+}

ec983419c1f6b1a97795b97b25214de809f7cd71
Dmitriy Bichenko <dmitriy.bichenko@playwing.net>
2019-09-11 17:36:04 +0300

    Additional functionality

---
+++ vars/appReviewInfoStoreValue.groovy
@@ -0,0 +1,31 @@
+def call(APP_ID, key, value){
+
+  def reviewInfo = appReviewInfoRetrieve(APP_ID);
+
+  def data;
+
+  if(!reviewInfo){
+    data = getDefaultValue();
+  }else{
+    data = reviewInfo;
+  }
+
+  data[key] = value
+
+  GString path = "/var/app/${APP_ID}/config/review.json"
+
+  writeToExternalJsonFile(path, data)
+
+}
+
+
+def getDefaultValue(){
+
+  def defaults = [
+      isReviewOk: false,
+      selectedReviewer: null
+  ]
+
+  return  defaults;
+
+}
